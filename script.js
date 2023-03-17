cc3136e8917f111a5d0855d2015ae0f3ebfcf773
Fabio Morais <127254770+devfabiomorais@users.noreply.github.com>
2023-03-17 16:35:12 +0000

    Introduzi música de fundo e efeitos de áudio para o pulo e para o game over.

--- script.js
+++ script.js
@@ -1,27 +1,52 @@
 const mario = document.querySelector('.mario');
 const pipe = document.querySelector('.pipe');
-  
-const jump = () => {
-  mario.classList.add('jump');
 
+
+function playAudio(){
+  let x = document.getElementById("musicadefundo");
+  x.play();
+}
+
+function pauseAudio(){
+  let x = document.getElementById("musicadefundo");
+  x.pause();
+}
+
+function audioPerdeu(){
+  let x = document.getElementById("perdeu");
+  x.play();
+}
+
+function audioPulou(){
+  let x = document.getElementById("pulou");
+  x.play();
+}
+
+const jump = () => { 
+  audioPulou();
+  playAudio();
+  mario.classList.add('jump');
+  
   setTimeout(() => {
     mario.classList.remove('jump');
 } , 500);
 }
 
 const loop = setInterval(() => {
-  
+   
   const pipePosition = pipe.offsetLeft;
   const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '');
 
   if (pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
+    pauseAudio();
+    audioPerdeu();
     pipe.style.animation = 'none';
     pipe.style.left = `${pipePosition}px`;
 
     mario.style.animation = 'none';
     mario.style.bottom = `${marioPosition}px`;
 
-    mario.src = './images/game-over.png';
+    mario.src = './images/game-over.png';    
     mario.style.width = '75px';
     mario.style.marginLeft = '50px';
     
